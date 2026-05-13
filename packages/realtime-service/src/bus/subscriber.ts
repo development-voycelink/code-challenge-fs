1cb110034c8a2a2f79a70ff4653637796c292f0c
Leonardo <leonardo.estacio.h@gmail.com>
2026-05-12 19:16:33 -0500

    feat: add Redis subscription for call status updates

--- packages/realtime-service/src/bus/subscriber.ts
+++ packages/realtime-service/src/bus/subscriber.ts
@@ -1,13 +1,25 @@
-import Redis from 'ioredis';
-import { CallStatusUpdate } from '../types';
+import Redis from "ioredis";
+import { config } from "../config";
+import { CallStatusUpdate } from "../types";
 
-const CHANNEL = 'call-status-updates';
+const CHANNEL = "call-status-updates";
 
 export function subscribeToCallUpdates(
   onUpdate: (update: CallStatusUpdate) => void,
 ): void {
-  // TODO: subscribe to CHANNEL on Redis and call onUpdate for each message
-  void Redis;
-  void CHANNEL;
-  void onUpdate;
+  const sub = new Redis(config.redisUrl);
+
+  sub.subscribe(CHANNEL, (err) => {
+    if (err) console.error("[redis] subscribe error:", err);
+    else console.log(`[redis] subscribed to ${CHANNEL}`);
+  });
+
+  sub.on("message", (_, message) => {
+    try {
+      const update = JSON.parse(message) as CallStatusUpdate;
+      onUpdate(update);
+    } catch (err) {
+      console.error("[redis] failed to parse message:", err);
+    }
+  });
 }

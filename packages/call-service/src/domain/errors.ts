da60ba0f58ea484393fe7442c676722ea2729c00
Leonardo <leonardo.estacio.h@gmail.com>
2026-05-12 19:14:02 -0500

    feat: add custom error classes

---
+++ packages/call-service/src/domain/errors.ts
@@ -0,0 +1,15 @@
+export class NotFoundError extends Error {
+  readonly statusCode = 404;
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotFoundError';
+  }
+}
+
+export class InvalidTransitionError extends Error {
+  readonly statusCode = 422;
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalidTransitionError';
+  }
+}

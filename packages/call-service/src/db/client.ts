33085b07bdcdd696c51b675774d2bdd0fd4dbef5
Leonardo <leonardo.estacio.h@gmail.com>
2026-05-12 19:12:53 -0500

    fix: export database client

--- packages/call-service/src/db/client.ts
+++ packages/call-service/src/db/client.ts
@@ -2,4 +2,4 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 import { config } from "../config";
 
-const db = drizzle(config.databaseUrl, { schema });
+export const db = drizzle(config.databaseUrl, { schema });
